a605f4eb0566df83961d37d07e187bb5a444bb03
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-11-27 21:05:56 +0800

    fix

--- tools/lbaf23/dist_train.sh
+++ tools/lbaf23/dist_train.sh
@@ -5,6 +5,7 @@ GPUS=$2
 PORT=${PORT:-29500}
 
 MKL_SERVICE_FORCE_INTEL=1 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+srun --gres=gpu:V100:1 --time 2800 \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/../train.py $CONFIG --launcher pytorch ${@:3}
 # Any arguments from the third one are captured by ${@:3}
