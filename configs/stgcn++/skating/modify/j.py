a05255eec67eb61488b22ec30477d0dad3ead499
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-12-03 16:44:16 +0800

    Remove val

--- configs/stgcn++/skating/modify/j.py
+++ configs/stgcn++/skating/modify/j.py
@@ -20,22 +20,13 @@ train_pipeline = [
     dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['keypoint'])
 ]
-val_pipeline = [
-    # dict(type='PreNormalize2D'),
-    dict(type='GenSkeFeat', dataset='skating', feats=['j']),
-    dict(type='UniformSample', clip_len=clip_len, num_clips=1, test_mode=True),
-    dict(type='PoseDecode'),
-    dict(type='FormatGCNInput', num_person=1),
-    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['keypoint'])
-]
 test_pipeline = [
     # dict(type='PreNormalize2D'),
     dict(type='GenSkeFeat', dataset='skating', feats=['j']),
     dict(type='UniformSample', clip_len=clip_len, num_clips=4, test_mode=True),
     dict(type='PoseDecode'),
     dict(type='FormatGCNInput', num_person=1),
-    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
+    dict(type='Collect', keys=['keypoint'], meta_keys=[]),
     dict(type='ToTensor', keys=['keypoint'])
 ]
 data = dict(
@@ -46,8 +37,8 @@ data = dict(
         type='RepeatDataset',
         times=5,
         dataset=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='train')),
-    val=dict(type=dataset_type, ann_file=ann_file, pipeline=val_pipeline, split='val'),
-    test=dict(type=dataset_type, ann_file=ann_file, pipeline=test_pipeline, split='test'))
+    test=dict(type=dataset_type, ann_file=ann_file, pipeline=test_pipeline, split='test')
+)
 
 # optimizer
 optimizer = dict(type='SGD', lr=0.2, momentum=0.9, weight_decay=0.0005, nesterov=True)
